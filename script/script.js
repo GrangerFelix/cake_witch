75048b9aa785d918a066897aa7e505250d641925
GrangerFelix <91159083+GrangerFelix@users.noreply.github.com>
2021-09-22 17:11:53 +0300

    новый дизайн
    другое лого, расположение элементов + файл скрипта

---
+++ script/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	button.onclick = function() {
+		document.getElementById('homePage').classList.remove('homePage');
+		document.getElementById('homePage').classList.add('displayNone');
+		document.getElementById('secondPage').classList.remove('displayNone');
+		alert("zhopa");
+	};
+	alert("sraka");
+
+
+
+
+
+};
