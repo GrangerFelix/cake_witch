3657a7ca20a2d09fd35b2dba5f40932fc73bc1ef
GrangerFelix <91159083+GrangerFelix@users.noreply.github.com>
2021-09-22 19:16:44 +0300

    переключение страниц

--- public/static/script.js
+++ public/static/script.js
@@ -2,9 +2,15 @@ window.onload = function() {
 	button.onclick = function() {
 		document.getElementById('homePage').classList.remove('homePage');
 		document.getElementById('homePage').classList.add('displayNone');
-		/*document.getElementById('secondPage').classList.remove('displayNone');*/
 		secondPage.style.display = "flex";
 		alert("zhopa");
+	};
+	buttonSP1.onclick = function() {
+		secondPage.style.display = "none";
+		document.getElementById('homePage').classList.add('homePage');
+	};
+	buttonSP2.onclick = function() {
+		secondPage.style.display = "none";
 	};
 	alert("sraka");
 
