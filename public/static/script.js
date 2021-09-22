f3cfc9f09e6107467e5bdbf107516f876461cd3f
GrangerFelix <91159083+GrangerFelix@users.noreply.github.com>
2021-09-22 18:52:06 +0300

    вторая страница

--- public/static/script.js
+++ public/static/script.js
@@ -2,7 +2,8 @@ window.onload = function() {
 	button.onclick = function() {
 		document.getElementById('homePage').classList.remove('homePage');
 		document.getElementById('homePage').classList.add('displayNone');
-		document.getElementById('secondPage').classList.remove('displayNone');
+		/*document.getElementById('secondPage').classList.remove('displayNone');*/
+		secondPage.style.display = "flex";
 		alert("zhopa");
 	};
 	alert("sraka");
