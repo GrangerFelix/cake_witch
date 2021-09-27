48debd714eb0e059b00d1fa2a9927e3833d99ef3
GrangerFelix <91159083+GrangerFelix@users.noreply.github.com>
2021-09-27 15:50:40 +0300

    новые переходы между страницами

--- public/static/script.js
+++ public/static/script.js
@@ -1,13 +1,15 @@
 window.onload = function() {
 	button.onclick = function() {
-		document.getElementById('homePage').classList.remove('homePage');
-		document.getElementById('homePage').classList.add('displayNone');
+		/*document.getElementById('homePage').classList.remove('homePage');
+		document.getElementById('homePage').classList.add('displayNone');*/
+		homePage.style.display = "none";
 		secondPage.style.display = "flex";
 		/*alert("zhopa");*/
 	};
 	menu.onclick = function() {
-		document.getElementById('homePage').classList.remove('homePage');
-		document.getElementById('homePage').classList.add('displayNone');
+		/*document.getElementById('homePage').classList.remove('homePage');
+		document.getElementById('homePage').classList.add('displayNone');*/
+		homePage.style.display = "none";
 		menuPage.style.display = "flex";
 	}
 	menu1.onclick = function() {
@@ -27,6 +29,19 @@ window.onload = function() {
 		secondPage.style.display = "none";
 		thirdPage.style.display = "flex";
 	};
+	buttonMP1.onclick = function() {
+		menuPage.style.display = "none";
+		homePage.style.display = "flex";
+	}
+	menuSP.onclick = function() {
+		secondPage.style.display = "none";
+		menuPage.style.display = "flex";
+	}
+	homeP.onclick = function() {
+		secondPage.style.display = "none";
+		homePage.style.display = "flex";
+	}
+
 	buttonTP1.onclick = function() {
 		thirdPage.style.display = "none";
 		secondPage.style.display = "flex";
